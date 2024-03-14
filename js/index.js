f4a982241e44503125db53f5a324d6f3f450a6eb
bhardwajsaurav <saurav.bhardwaj@ekaleido.co>
2024-03-14 12:07:17 +0530

    update ui

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:1
+        1000: {
+            items: 1
         }
     }
 })
+
+
+
+function scrollMy() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+
+
+var distance = $('.overview').offset().top;
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= distance) {
+        $(".upscroll").show();
+    } else {
+        $(".upscroll").hide()
+    }
+});
